d03847596dd316de51964699bc43a5c3a518ec97
Dmytro Maslakov <dmitry.maslakov@gmail.com>
2025-07-28 03:55:35 +0300

    Update connect.py

--- HomeWork_08/connect.py
+++ HomeWork_08/connect.py
@@ -4,8 +4,6 @@ import os
 # Важливо: Замініть YOUR_USERNAME, YOUR_PASSWORD, YOUR_CLUSTER_URL та YOUR_DB_NAME на ваші реальні дані з MongoDB Atlas.
 # Отримайте URL підключення з змінної оточення або встановіть за замовчуванням
 # Рекомендується використовувати змінні оточення для конфіденційних даних
-# MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://YOUR_USERNAME:YOUR_PASSWORD@YOUR_CLUSTER_URL/YOUR_DB_NAME?retryWrites=true&w=majority")
-# MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://mitus66:Alaska725@cluster0.qzzbjox.mongodb.net/mongodb_1?retryWrites=true&w=majority")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://mitus66:Alaska725@cluster0.qzzbjox.mongodb.net/?retryWrites=true&w=majority")
 
 def connect_db():
