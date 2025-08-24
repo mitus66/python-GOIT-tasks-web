616d82325a5ebac95129e715b3324ef8350a54b4
Dmytro Maslakov <dmitry.maslakov@gmail.com>
2025-08-24 14:41:47 +0300

    add else

---
+++ HomeWork_10/quotes_project/quotes_project/urls.py
@@ -0,0 +1,10 @@
+# quotes_project/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('quotes.urls')),
+    path('users/', include('users.urls')),  # Додайте цей рядок
+]
